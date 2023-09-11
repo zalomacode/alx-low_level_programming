1d68e28fc969bb5d8a83f5e53f39dc10e5b69598
iSalmaa <saljoazarpeyman@gmail.com>
2023-09-11 04:42:51 -0700

    a

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: print 16base
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
