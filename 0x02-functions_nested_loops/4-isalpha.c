080c300acabc1dc348d05b496deff1d319cf6759
iSalmaa <saljoazarpeyman@gmail.com>
2023-09-12 16:09:12 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,9 +11,13 @@
 
 int _isalpha(int c)
 {
-	if (c <= 97 && c <= 122)
+	if ((c <= 97 && c <= 122) || if (c >= 65 && c <=  90))
+	{
 		return (1);
-	if (c >= 65 && c <=  90)
+	}
+	else
+	{
 		return (0);
+	}
 	return (0);
 }
