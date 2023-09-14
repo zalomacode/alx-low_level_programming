d02a002aeac8954e47a402905fc9d2398b53b982
iSalmaa <saljoazarpeyman@gmail.com>
2023-09-14 13:27:59 -0700

    a

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,14 @@
 #include "main.h"
 /**
-*more_numbers - prints numbers to 14
-*
-*Return: returns nothing
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
 */
-void more_numbers(void)
+void print_line(int n)
 {
-int number1, number2;
-
-for (number1 = 0; number1 < 10; number1++)
+while (n-- > 0)
 {
-for (number2 = 0; number2 <= 14; number2++)
-
-{
-if (number2 > 9)
-{
-_putchar((number2 / 10) + '0');
+_putchar('_');
 }
-_putchar((number2 % 10) + '0');
-}
-_putchar(10);
-}
+_putchar('\n');
 }
